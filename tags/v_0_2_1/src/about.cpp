738f9214cc487c55731db6a835fd0c25a8bd92cc
(no author) <(no author)@64c129e3-8119-4a17-9932-72cb407d3134>
2008-10-11 09:56:48 +0000

    This commit was manufactured by cvs2svn to create tag 'v_0_2_1'.
    git-svn-id: https://vmpk.svn.sourceforge.net/svnroot/vmpk@41 64c129e3-8119-4a17-9932-72cb407d3134

---
+++ tags/v_0_2_1/src/about.cpp
@@ -0,0 +1,44 @@
+/*
+    MIDI Virtual Piano Keyboard 
+    Copyright (C) 2008, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along 
+    with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "about.h"
+
+About::About(QWidget *parent)
+    : QDialog(parent)
+{
+    const QString PGM_VERSION(VERSION);
+    const QString BLD_DATE(__DATE__);
+    const QString BLD_TIME(__TIME__);
+    const QString CMP_VERSION(__VERSION__);
+    
+    ui.setupUi(this);
+    ui.labelVersion->setText(tr("<html>"
+            "<head>"
+              "<meta name=\"qrichtext\" content=\"1\" />"
+              "<style type=\"text/css\">\np, li { white-space: pre-wrap; }</style>"
+            "</head>"
+            "<body style=\"font-family:'Sans Serif'; font-size:12pt; font-weight:400; font-style:normal;\">"
+              "<p style=\"margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">"
+                "Version: %1<br/>"
+                "Build date: %2<br/>"
+                "Build time: %3<br/>"
+                "Compiler: %4" 
+              "</p>"
+            "</body>"
+            "</html>").arg(PGM_VERSION, BLD_DATE, BLD_TIME, CMP_VERSION));
+}
