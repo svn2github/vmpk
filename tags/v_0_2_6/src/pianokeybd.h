caaa8ab6a82fc67f17eacf95e8ca7a7f4880bcba
(no author) <(no author)@64c129e3-8119-4a17-9932-72cb407d3134>
2009-08-02 21:12:37 +0000

    This commit was manufactured by cvs2svn to create tag 'v_0_2_6'.
    git-svn-id: https://vmpk.svn.sourceforge.net/svnroot/vmpk@98 64c129e3-8119-4a17-9932-72cb407d3134

---
+++ tags/v_0_2_6/src/pianokeybd.h
@@ -0,0 +1,85 @@
+/*
+    MIDI Virtual Piano Keyboard
+    Copyright (C) 2008-2009, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef PIANOKEYBD_H
+#define PIANOKEYBD_H
+
+#include "pianoscene.h"
+#include <QGraphicsView>
+//#include <QtDesigner/QDesignerExportWidget>
+
+//class QDESIGNER_WIDGET_EXPORT PianoKeybd : public QGraphicsView
+class PianoKeybd : public QGraphicsView
+{
+    Q_OBJECT
+    Q_PROPERTY( int baseOctave READ baseOctave WRITE setBaseOctave )
+    Q_PROPERTY( int numOctaves READ numOctaves WRITE setNumOctaves )
+    Q_PROPERTY( int rotation READ getRotation WRITE setRotation )
+    Q_PROPERTY( QColor keyPressedColor READ getKeyPressedColor WRITE setKeyPressedColor )
+    Q_PROPERTY( bool showLabels READ showLabels WRITE setShowLabels )
+    Q_PROPERTY( bool useFlats READ useFlats WRITE setUseFlats )
+    Q_PROPERTY( int transpose READ getTranspose WRITE setTranspose )
+
+public:
+    PianoKeybd(QWidget *parent = 0);
+    PianoKeybd(const int baseOctave, const int numOctaves, QWidget *parent = 0);
+    void setKeyboardMap(KeyboardMap* m) { m_scene->setKeyboardMap(m); }
+    KeyboardMap* getKeyboardMap() { return m_scene->getKeyboardMap(); }
+    int baseOctave() const { return m_scene->baseOctave(); }
+    int numOctaves() const { return m_scene->numOctaves(); }
+    void setBaseOctave(const int baseOctave) { m_scene->setBaseOctave(baseOctave); }
+    void setNumOctaves(const int numOctaves);
+    QSize sizeHint() const;
+    int getRotation() const { return m_rotation; }
+    void setRotation(int r);
+    QColor getKeyPressedColor() const { return m_scene->getKeyPressedColor(); }
+    void setKeyPressedColor(const QColor& c) { m_scene->setKeyPressedColor(c); }
+    void allKeysOff() { m_scene->allKeysOff(); }
+    void setPianoHandler(PianoHandler* handler) { m_scene->setPianoHandler(handler); }
+    int minNote() const { return m_scene->getMinNote(); }
+    void setMinNote(int n) { m_scene->setMinNote(n); }
+    int maxNote() const { return m_scene->getMaxNote(); }
+    void setMaxNote(int n) { m_scene->setMaxNote(n); }
+    int getTranspose() const { return m_scene->getTranspose(); }
+    void setTranspose(int t) { m_scene->setTranspose(t); }
+    bool showLabels() const { return m_scene->showLabels(); }
+    void setShowLabels(bool show) { m_scene->setShowLabels(show); }
+    bool useFlats() const { return m_scene->useFlats(); }
+    void setUseFlats(bool use) { m_scene->setUseFlats(use); }
+
+public slots:
+    void showNoteOn( int midiNote );
+    void showNoteOff( int midiNote );
+
+signals:
+    void noteOn( int midiNote );
+    void noteOff( int midiNote );
+
+protected:
+    void initialize();
+    void initDefaultMap();
+    void initScene(int base, int num, const QColor& c = QColor());
+    void resizeEvent(QResizeEvent *event);
+
+private:
+    int m_rotation;
+    PianoScene *m_scene;
+    KeyboardMap m_defaultMap;
+};
+
+#endif // PIANOKEYBD_H
---
+++ tags/v_0_2_6/vmpk.pro
@@ -0,0 +1,114 @@
+# Virtual MIDI Piano Keyboard
+# Copyright (C) 2008 Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along 
+# with this program; If not, see <http://www.gnu.org/licenses/>.
+
+TEMPLATE = app
+TARGET = vmpk
+VERSION = 0.2.6cvs
+DESTDIR = build
+OBJECTS_DIR = build
+UI_DIR = build
+MOC_DIR = build
+RCC_DIR = build
+QT += core gui xml
+CONFIG += release
+CONFIG -= debug_and_release
+CONFIG -= debug
+VERSIONH = $$sprintf(const QString PGM_VERSION(\"%1\");,$$VERSION)
+win32 {
+    DEFINES += __WINDOWS_MM__
+    LIBS += -lwinmm
+    RC_FILE = src/vpianoico.rc
+    system(echo $$VERSIONH > $$DESTDIR/vmpk_version.h)
+}
+linux* { 
+    DEFINES += __LINUX_ALSASEQ__
+    DEFINES += AVOID_TIMESTAMPING
+    CONFIG += link_pkgconfig
+    PKGCONFIG += alsa
+    system(echo \'$$VERSIONH\' > $$DESTDIR/vmpk_version.h)
+}
+macx {
+    CONFIG += x86 ppc
+    ICON = data/vmpk.icns
+    DEFINES += __MACOSX_CORE__
+    BUNDLE_RES.files = data/help.html \
+	  data/help_es.html \
+	  data/help_tr.html \
+        data/gmgsxg.ins \
+        data/spanish.xml \
+        data/german.xml \
+        data/azerty.xml \
+        data/it-qwerty.xml \
+        data/vkeybd-default.xml \
+        translations/vmpk_es.qm \
+        translations/vmpk_tr.qm
+    BUNDLE_RES.path = Contents/Resources
+    QMAKE_BUNDLE_DATA += BUNDLE_RES
+    LIBS += -framework CoreMidi -framework CoreAudio -framework CoreFoundation
+    system(echo \'$$VERSIONH\' > $$DESTDIR/vmpk_version.h)
+}
+irix* { 
+    DEFINES += __IRIX_MD__
+    LIBS += -laudio -lpthread
+    system(echo \'$$VERSIONH\' > $$DESTDIR/vmpk_version.h)
+}
+debug:DEFINES += __RTMIDI_DEBUG__
+INCLUDEPATH += src
+# Input
+FORMS += src/kmapdialog.ui \
+    src/midisetup.ui \
+    src/vpiano.ui \
+    src/about.ui \
+    src/preferences.ui \
+    src/riffimportdlg.ui
+HEADERS += src/kmapdialog.h \
+    src/keyboardmap.h \
+    src/mididefs.h \
+    src/instrument.h \
+    src/midisetup.h \
+    src/RtError.h \
+    src/RtMidi.h \
+    src/knob.h \
+    src/pianokey.h \
+    src/pianokeybd.h \
+    src/pianoscene.h \
+    src/classicstyle.h \
+    src/vpiano.h \
+    src/about.h \
+    src/preferences.h \
+    src/riff.h \
+    src/riffimportdlg.h \
+    src/instrument.h \
+    src/constants.h
+SOURCES += src/kmapdialog.cpp \
+    src/keyboardmap.cpp \
+    src/midisetup.cpp \
+    src/RtMidi.cpp \
+    src/knob.cpp \
+    src/pianokey.cpp \
+    src/pianokeybd.cpp \
+    src/pianoscene.cpp \
+    src/riff.cpp \
+    src/riffimportdlg.cpp \
+    src/classicstyle.cpp \
+    src/vpiano.cpp \
+    src/about.cpp \
+    src/preferences.cpp \
+    src/instrument.cpp \
+    src/main.cpp
+RESOURCES += data/vmpk.qrc
+TRANSLATIONS += translations/vmpk_es.ts \
+    translations/vmpk_tr.ts 
